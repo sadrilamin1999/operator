6e8099c45f83e10143064d518aaabfeeb5d9e5c2
Sadril Amin <sadrilshuvo420@gmail.com>
2023-02-18 16:15:56 +0600

    tringle length find out system added

---
+++ Length-of-tringle.c
@@ -0,0 +1,13 @@
+//area = aqrt(s*(s-a)*(s-b)*(s-c)
+//s= (a+b+c)/2
+
+#include<stdio.h>
+int main()
+{
+  double a,b,c,s; //squre rote always return a double type varibale
+  printf("Enter three value: ");
+  scanf("%lf%lf%lf", &a,&b,&c);
+  s=(a+b+c)/2;
+  printf("%lf",s);
+  getch();
+}
